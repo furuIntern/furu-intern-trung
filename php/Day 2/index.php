516272b8e9b01af70fe2c9347adac556e3cb396d
phuctrung0201 <mygalaxy2712@gmail.com>
2019-02-15 13:59:28 +0700

    Create heroku

---
+++ php/Day 2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container list-group">
+        <a href="./task1.html" class="list-group-item list-group-item-action">Task 1</a>
+        <a href="./task2.html" class="list-group-item list-group-item-action">Rask 2</a>
+        <a href="./task3.php" class="list-group-item list-group-item-action">Task 3</a>
+        <a href="./task4.php" class="list-group-item list-group-item-action">Task 4</a>
+        <a href="./task5.html" class="list-group-item list-group-item-action">Task 5</a>
+    </div>
+</body>1
+</html>
